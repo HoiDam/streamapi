1b9d96a56747046208cd118b7d8b5c2cbc237b06
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-11-30 14:06:33 +0800

    feat: finished filter/map/reduce/supplement functions (Total *12)

--- src/main/java/com/thoughtworks/collection/StreamReduce.java
+++ src/main/java/com/thoughtworks/collection/StreamReduce.java
@@ -5,14 +5,25 @@ import java.util.List;
 public class StreamReduce {
 
     public int getLastOdd(List<Integer> numbers) {
-        return 0;
+
+        return numbers.stream()
+                .filter(number -> number%2==1)
+                .reduce(0,(accum,number)-> {
+                    return number;
+                });
     }
 
     public String getLongest(List<String> words) {
-        return null;
+
+        return words.stream()
+                .reduce("",(identity,word)-> {
+                    return word.length() > identity.length() ? word : identity;
+                });
     }
 
     public int getTotalLength(List<String> words) {
-        return 0;
+        return words.stream()
+                .map(word -> word.length())
+                .reduce(0,(wordL,n) -> wordL+n);
     }
 }
